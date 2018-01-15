a6bdb50cdbd7df7a643b3ba46e4341488bedf208
Jayden - laptop <jaydenghunter96@gmail.com>
2018-01-15 22:05:05 +0800

    Update

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public int score;
+    public float power;
+    public float modifier;
+    public float nextRotation;
+    public float timeMod;
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = timeMod;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+        {
+            power++;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            rb.AddForce(new Vector3(-power*modifier,power*modifier,0));
+            power = 0;
+        }
+	}
+}
